c107ebcac637d75ba5d56b461a88dd51fe1aaacf
RKouchoo <r.kouchoo1@gmail.com>
2016-08-25 12:48:03 +1000

    updated movement even more
    removed unnecessary functions, will eventually be implemented to main class

--- updated_movement_control/updated_movement_control.ino
+++ updated_movement_control/updated_movement_control.ino
@@ -2,9 +2,7 @@ void MoveFORWARD() {
   digitalWrite(Motor1A, HIGH);
   digitalWrite(Motor2A, HIGH);
   digitalWrite(Motor3A, HIGH);
-  digitalWrite(Motor4B, HIGH);
-                                // digitalWrite(Motor3A, HIGH);
-                                // digitalWrite(Motor4A, HIGH);
+  digitalWrite(Motor4B, HIGH);                            // digitalWrite(Motor3A, HIGH);                              // digitalWrite(Motor4A, HIGH);
   delay(50);
   digitalWrite(Motor1A, LOW);   // digitalWrite(Motor3A, LOW);
   digitalWrite(Motor2A, LOW);   // digitalWrite(Motor4A, LOW);
@@ -22,33 +20,8 @@ void MoveBACKWARD() {
   digitalWrite(Motor3B, LOW);
   digitalWrite(Motor4A, LOW);
 }
-void MoveBACKWARDRIGHT() {
-  digitalWrite(Motor1B, HIGH);
-  digitalWrite(Motor3B, HIGH);
-  delay(50);
-  digitalWrite(Motor1B, LOW);
-  digitalWrite(Motor3B, LOW);
-}
-void MoveFORWARDRIGHT() {
-  digitalWrite(Motor1A, HIGH);
-  digitalWrite(Motor3A, HIGH);
-  delay(50);
-  digitalWrite(Motor1A, LOW);
-  digitalWrite(Motor3A, LOW);
-}
-void MoveFORWARDLEFT() {//
-  digitalWrite(Motor1A, HIGH);
-  digitalWrite(Motor3A, HIGH);
-  delay(50);
-  digitalWrite(Motor1A, LOW);
-  digitalWrite(Motor3A, LOW);
-}
-void MoveBACKWARDLEFT() {//
-  digitalWrite(Motor2B, HIGH);
-  digitalWrite(Motor4B, HIGH);
-  delay(50);
-  digitalWrite(Motor2B, LOW);
-  digitalWrite(Motor4B, LOW);
+
+
 }
 void MoveSIDEWAYSRIGHT() {//
   digitalWrite(Motor2A, HIGH);
